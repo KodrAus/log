f20cc335ed474c65e53946cecedda5f68791bb7b
Ashley Mannix <ashleymannix@live.com.au>
2018-10-23 14:37:35 +1000

    fix up macro

--- src/key_values/macros.rs
+++ src/key_values/macros.rs
@@ -2,7 +2,7 @@
 #[doc(hidden)]
 macro_rules! properties(
     ({ stream: [$($stream:tt)*], kvs_ident: $kvs_ident:ident }) => {
-        properties!(@ expect_adapter {
+        properties!(@ expect_key {
             stream: [$($stream)*],
             tokens: [],
             kvs_ident: $kvs_ident
@@ -10,72 +10,23 @@ macro_rules! properties(
     };
 
     // We're finished parsing
-    (@ expect_adapter {
+    (@ expect_key {
         stream: [],
         tokens: [$($tokens:tt)*],
         kvs_ident: $kvs_ident:ident
     }) => {
-        let $kvs_ident: &[(&str, &dyn $crate::key_values::ToValue)] = &[$($tokens)*];
-    };
-    // Munch a key identifier from the token stream
-    (@ expect_adapter {
-        stream: [$key:ident $($stream:tt)*],
-        tokens: [$($tokens:tt)*],
-        kvs_ident: $kvs_ident:ident
-    }) => {
-        properties!(@ expect_separator {
-            stream: [$($stream)*],
-            tokens: [$($tokens)*],
-            adapter: {
-                kind: default
-            },
-            key: $key,
-            kvs_ident: $kvs_ident
-        });
-    };
-    // Munch an attribute from the token stream
-    (@ expect_adapter {
-        stream: [#[log($adapter:ident)] $($stream:tt)*],
-        tokens: [$($tokens:tt)*],
-        kvs_ident: $kvs_ident:ident
-    }) => {
-        properties!(@ expect_key {
-            stream: [$($stream)*],
-            tokens: [$($tokens)*],
-            adapter: {
-                kind: $adapter
-            },
-            kvs_ident: $kvs_ident
-        });
-    };
-    // Munch an attribute from the token stream
-    (@ expect_adapter {
-        stream: [#[log($adapter_kind:ident = $adapter_state:expr)] $($stream:tt)*],
-        tokens: [$($tokens:tt)*],
-        kvs_ident: $kvs_ident:ident
-    }) => {
-        properties!(@ expect_key {
-            stream: [$($stream)*],
-            tokens: [$($tokens)*],
-            adapter: {
-                kind: $adapter_kind,
-                state: $adapter_state
-            },
-            kvs_ident: $kvs_ident
-        });
+        let $kvs_ident: &[(&str, &dyn $crate::key_values::Value)] = &[$($tokens)*];
     };
 
     // Munch a key as an identifier from the token stream
     (@ expect_key {
         stream: [$key:ident $($stream:tt)*],
         tokens: [$($tokens:tt)*],
-        adapter: { $($adapter:tt)* },
         kvs_ident: $kvs_ident:ident
     }) => {
         properties!(@ expect_separator {
             stream: [$($stream)*],
             tokens: [$($tokens)*],
-            adapter: { $($adapter)* },
             key: $key,
             kvs_ident: $kvs_ident
         });
@@ -85,14 +36,12 @@ macro_rules! properties(
     (@ expect_separator {
         stream: [= $($stream:tt)*],
         tokens: [$($tokens:tt)*],
-        adapter: { $($adapter:tt)* },
         key: $key:ident,
         kvs_ident: $kvs_ident:ident
     }) => {
         properties!(@ expect_value {
             stream: [$($stream)*],
             tokens: [$($tokens)*],
-            adapter: { $($adapter)* },
             key: $key,
             kvs_ident: $kvs_ident
         });
@@ -101,14 +50,12 @@ macro_rules! properties(
     (@ expect_separator {
         stream: [: $($stream:tt)*],
         tokens: [$($tokens:tt)*],
-        adapter: { $($adapter:tt)* },
         key: $key:ident,
         kvs_ident: $kvs_ident:ident
     }) => {
         properties!(@ expect_value {
             stream: [$($stream)*],
             tokens: [$($tokens)*],
-            adapter: { $($adapter)* },
             key: $key,
             kvs_ident: $kvs_ident
         });
@@ -118,14 +65,12 @@ macro_rules! properties(
     (@ expect_separator {
         stream: [, $($stream:tt)*],
         tokens: [$($tokens:tt)*],
-        adapter: { $($adapter:tt)* },
         key: $key:ident,
         kvs_ident: $kvs_ident:ident
     }) => {
         properties!(@ with_adapter {
             stream: [$($stream)*],
             tokens: [$($tokens)*],
-            adapter: { $($adapter)* },
             key: $key,
             value: $key,
             kvs_ident: $kvs_ident
@@ -136,14 +81,12 @@ macro_rules! properties(
     (@ expect_separator {
         stream: [],
         tokens: [$($tokens:tt)*],
-        adapter: { $($adapter:tt)* },
         key: $key:ident,
         kvs_ident: $kvs_ident:ident
     }) => {
         properties!(@ with_adapter {
             stream: [],
             tokens: [$($tokens)*],
-            adapter: { $($adapter)* },
             key: $key,
             value: $key,
             kvs_ident: $kvs_ident
@@ -154,14 +97,12 @@ macro_rules! properties(
     (@ expect_value {
         stream: [$value:expr , $($stream:tt)*],
         tokens: [$($tokens:tt)*],
-        adapter: { $($adapter:tt)* },
         key: $key:ident,
         kvs_ident: $kvs_ident:ident
     }) => {
-        properties!(@ with_adapter {
+        properties!(@ with_value {
             stream: [$($stream)*],
             tokens: [$($tokens)*],
-            adapter: { $($adapter)* },
             key: $key,
             value: $value,
             kvs_ident: $kvs_ident
@@ -171,60 +112,12 @@ macro_rules! properties(
     (@ expect_value {
         stream: [$value:expr],
         tokens: [$($tokens:tt)*],
-        adapter: { $($adapter:tt)* },
         key: $key:ident,
         kvs_ident: $kvs_ident:ident
     }) => {
-        properties!(@ with_adapter {
+        properties!(@ with_value {
             stream: [],
             tokens: [$($tokens)*],
-            adapter: { $($adapter)* },
-            key: $key,
-            value: $value,
-            kvs_ident: $kvs_ident
-        });
-    };
-
-    // Use the adapter and replace with the default (no-op)
-    // The adapter is a function like `T -> impl ToValue`
-    (@ with_adapter {
-        stream: [$($stream:tt)*],
-        tokens: [$($tokens:tt)*],
-        adapter: {
-            kind: $adapter_kind:ident
-        },
-        key: $key:ident,
-        value: $value:expr,
-        kvs_ident: $kvs_ident:ident
-    }) => {
-        properties!(@ with_value {
-            stream: [$($stream)*],
-            tokens: [$($tokens)*],
-            adapter_fn: $crate::key_values::adapter::map::$adapter_kind,
-            key: $key,
-            value: $value,
-            kvs_ident: $kvs_ident
-        });
-    };
-    // Use the adapter and replace with the default (no-op)
-    // The adapter is a function like `(T, F: impl Fn(&T) -> fmt::Result) -> impl ToValue`
-    (@ with_adapter {
-        stream: [$($stream:tt)*],
-        tokens: [$($tokens:tt)*],
-        adapter: {
-            kind: $adapter_kind:ident,
-            state: $adapter_state:expr
-        },
-        key: $key:ident,
-        value: $value:expr,
-        kvs_ident: $kvs_ident:ident
-    }) => {
-        properties!(@ with_value {
-            stream: [$($stream)*],
-            tokens: [$($tokens)*],
-            adapter_fn: |value| {
-                $crate::key_values::adapter::map_with::$adapter_kind(value, $adapter_state)
-            },
             key: $key,
             value: $value,
             kvs_ident: $kvs_ident
@@ -237,15 +130,13 @@ macro_rules! properties(
     (@ with_value {
         stream: [$($stream:tt)*],
         tokens: [$($tokens:tt)*],
-        adapter_fn: $adapter_fn:expr,
         key: $key:ident,
         value: $value:expr,
         kvs_ident: $kvs_ident:ident
     }) => {
         let $key = &$value;
-        let $key = $adapter_fn($key);
 
-        properties!(@ expect_adapter {
+        properties!(@ expect_key {
             stream: [$($stream)*],
             tokens: [
                 $($tokens)*
